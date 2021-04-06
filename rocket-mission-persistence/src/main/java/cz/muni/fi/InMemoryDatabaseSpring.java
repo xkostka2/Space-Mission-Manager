6c662be85bb08efc57f786bbde8ff2b3365fd91b
Martin Kazimir <xkazimir@fi.muni.cz>
2021-04-06 13:17:27 +0200

    pom updated and ComponentDaoImpl tests created

--- rocket-mission-persistence/src/main/java/cz/muni/fi/InMemoryDatabaseSpring.java
+++ rocket-mission-persistence/src/main/java/cz/muni/fi/InMemoryDatabaseSpring.java
@@ -1,12 +1,16 @@
 package cz.muni.fi;
 
+import cz.muni.fi.dao.ComponentDao;
+import cz.muni.fi.entity.Component;
 import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
 import org.springframework.instrument.classloading.LoadTimeWeaver;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -19,19 +23,19 @@ import javax.sql.DataSource;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories
-@ComponentScan(basePackages = "cz.muni.fi")
+@ComponentScan(basePackageClasses = {Component.class}, basePackages = "cz.muni.fi")
 public class InMemoryDatabaseSpring {
 
     @Bean
-    public JpaTransactionManager transactionManager(){
-        return  new JpaTransactionManager(entityManagerFactory().getObject());
+    public JpaTransactionManager transactionManager() {
+        return new JpaTransactionManager(entityManagerFactory().getObject());
     }
 
     /**
      * Starts up a container that emulates behavior prescribed in JPA spec for container-managed EntityManager
      */
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean jpaFactoryBean = new LocalContainerEntityManagerFactoryBean();
         jpaFactoryBean.setDataSource(db());
         jpaFactoryBean.setLoadTimeWeaver(instrumentationLoadTimeWeaver());
@@ -40,17 +44,19 @@ public class InMemoryDatabaseSpring {
     }
 
     @Bean
-    public LocalValidatorFactoryBean localValidatorFactoryBean(){
+    public LocalValidatorFactoryBean localValidatorFactoryBean() {
         return new LocalValidatorFactoryBean();
     }
+
     @Bean
     public LoadTimeWeaver instrumentationLoadTimeWeaver() {
         return new InstrumentationLoadTimeWeaver();
     }
 
     @Bean
-    public DataSource db(){
+    public DataSource db() {
         EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        return builder.setType(EmbeddedDatabaseType.DERBY).build();
+        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.DERBY).build();
+        return db;
     }
 }
--- rocket-mission-persistence/src/test/java/cz/muni/fi/dao/ComponentDaoImplTest.java
+++ rocket-mission-persistence/src/test/java/cz/muni/fi/dao/ComponentDaoImplTest.java
@@ -1,18 +1,91 @@
 package cz.muni.fi.dao;
 
+import cz.muni.fi.InMemoryDatabaseSpring;
+import cz.muni.fi.entity.Component;
+import cz.muni.fi.enums.ComponentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Tests for ComponentDao implementation class.
  *
  * @author Martin Kazimir
  */
-@ContextConfiguration(classes = cz.muni.fi.InMemoryDatabaseSpring.class)
-public class ComponentDaoImplTest {
+@ContextConfiguration(classes = InMemoryDatabaseSpring.class)
+public class ComponentDaoImplTest extends AbstractTestNGSpringContextTests {
 
     @Autowired
     private ComponentDao componentDao;
 
+    private Component rocketBooster;
+    private Component foodSupplies;
 
+    @BeforeClass
+    public void beforeClass() {
+        rocketBooster = new Component();
+        rocketBooster.setName("rocket booster");
+        rocketBooster.setType(ComponentType.ROCKET);
+        componentDao.addComponent(rocketBooster);
+
+        foodSupplies = new Component();
+        foodSupplies.setName("food supplies");
+        foodSupplies.setType(ComponentType.MISSION);
+        componentDao.addComponent(foodSupplies);
+    }
+
+    @AfterClass
+    public void afterClass() {
+        for (Component component : componentDao.findAllComponents()) {
+            componentDao.removeComponent(component);
+        }
+    }
+
+    @Test
+    public void testAddComponent() {
+        Component stabilizer = new Component();
+        stabilizer.setName("stabilizer");
+        stabilizer.setType(ComponentType.ROCKET);
+        componentDao.addComponent(stabilizer);
+
+        assertThat(componentDao.findComponentById(stabilizer.getId())).isEqualTo(stabilizer);
+    }
+
+    @Test
+    public void testFindAllComponents() {
+        List<Component> allComponents = componentDao.findAllComponents();
+
+        assertThat(allComponents.size()).isGreaterThanOrEqualTo(2);
+        assertThat(allComponents.contains(rocketBooster)).isTrue();
+        assertThat(allComponents.contains(foodSupplies)).isTrue();
+    }
+
+    @Test
+    public void testFindComponentById() {
+        Component foundComponent = componentDao.findComponentById(rocketBooster.getId());
+
+        assertThat(foundComponent).isEqualTo(rocketBooster);
+    }
+
+    @Test
+    public void testUpdateComponent() {
+        rocketBooster.setName("solid rocket booster");
+        componentDao.updateComponent(rocketBooster);
+
+        assertThat(componentDao.findComponentById(rocketBooster.getId())).isEqualTo(rocketBooster);
+    }
+
+    @Test
+    public void testRemoveComponent() {
+        Long foodSuppliesId = foodSupplies.getId();
+        componentDao.removeComponent(foodSupplies);
+
+        assertThat(componentDao.findComponentById(foodSuppliesId)).isNull();
+    }
 }
