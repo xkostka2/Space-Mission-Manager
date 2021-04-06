4c9b22c21ac534c2dc839bc1d603fc6b9036faa0
Martin Kazimir <xkazimir@fi.muni.cz>
2021-04-06 21:00:03 +0200

    added tests for ComponentDaoImpl

--- rocket-mission-persistence/src/test/java/cz/muni/fi/dao/ComponentDaoImplTest.java
+++ rocket-mission-persistence/src/test/java/cz/muni/fi/dao/ComponentDaoImplTest.java
@@ -10,6 +10,9 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,14 +32,19 @@ public class ComponentDaoImplTest extends AbstractTestNGSpringContextTests {
 
     @BeforeClass
     public void beforeClass() {
+        LocalDateTime localDateTime = LocalDateTime.now();
         rocketBooster = new Component();
         rocketBooster.setName("rocket booster");
         rocketBooster.setType(ComponentType.ROCKET);
+        rocketBooster.setReadyDate(localDateTime.plusMonths(6).atZone(ZoneId.of("GMT")));
+        rocketBooster.setReadyToUse(false);
         componentDao.addComponent(rocketBooster);
 
         foodSupplies = new Component();
         foodSupplies.setName("food supplies");
         foodSupplies.setType(ComponentType.MISSION);
+        foodSupplies.setReadyDate(localDateTime.plusMonths(5).atZone(ZoneId.of("GMT")));
+        foodSupplies.setReadyToUse(false);
         componentDao.addComponent(foodSupplies);
     }
 
@@ -52,9 +60,36 @@ public class ComponentDaoImplTest extends AbstractTestNGSpringContextTests {
         Component stabilizer = new Component();
         stabilizer.setName("stabilizer");
         stabilizer.setType(ComponentType.ROCKET);
+        stabilizer.setReadyDate(LocalDateTime.now().atZone(ZoneId.of("GMT")));
+        stabilizer.setReadyToUse(false);
         componentDao.addComponent(stabilizer);
 
         assertThat(componentDao.findComponentById(stabilizer.getId())).isEqualTo(stabilizer);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testAddNullComponent() {
+        componentDao.addComponent(null);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testAddComponentNullName() {
+        Component stabilizer = new Component();
+        stabilizer.setType(ComponentType.ROCKET);
+        stabilizer.setReadyDate(LocalDateTime.now().atZone(ZoneId.of("GMT")));
+        stabilizer.setReadyToUse(false);
+        componentDao.addComponent(stabilizer);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testAddComponentNotNullId() {
+        Component stabilizer = new Component();
+        stabilizer.setName("stabilizer");
+        stabilizer.setType(ComponentType.ROCKET);
+        stabilizer.setReadyDate(LocalDateTime.now().atZone(ZoneId.of("GMT")));
+        stabilizer.setReadyToUse(false);
+        stabilizer.setId(10L);
+        componentDao.addComponent(stabilizer);
     }
 
     @Test
@@ -73,6 +108,23 @@ public class ComponentDaoImplTest extends AbstractTestNGSpringContextTests {
         assertThat(foundComponent).isEqualTo(rocketBooster);
     }
 
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testFindComponentByIdNullId() {
+        componentDao.findComponentById(null);
+    }
+
+    @Test
+    public void testFindNonExistingComponent() {
+        List<Component> allComponents = componentDao.findAllComponents();
+        Component max = allComponents.
+                stream().
+                max(Comparator.comparing(Component::getId)).get();
+
+        Component foundComponent = componentDao.findComponentById(max.getId() + 1);
+
+        assertThat(foundComponent).isNull();
+    }
+
     @Test
     public void testUpdateComponent() {
         rocketBooster.setName("solid rocket booster");
@@ -81,11 +133,39 @@ public class ComponentDaoImplTest extends AbstractTestNGSpringContextTests {
         assertThat(componentDao.findComponentById(rocketBooster.getId())).isEqualTo(rocketBooster);
     }
 
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testUpdateNullComponent() {
+        componentDao.updateComponent(null);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testUpdateComponentNullName() {
+        rocketBooster.setName(null);
+        componentDao.updateComponent(rocketBooster);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testUpdateComponentNullId() {
+        rocketBooster.setId(null);
+        componentDao.updateComponent(rocketBooster);
+    }
+
     @Test
     public void testRemoveComponent() {
         Long foodSuppliesId = foodSupplies.getId();
         componentDao.removeComponent(foodSupplies);
 
         assertThat(componentDao.findComponentById(foodSuppliesId)).isNull();
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testRemoveNullComponent() {
+        componentDao.removeComponent(null);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testRemoveComponentNullId() {
+        foodSupplies.setId(null);
+        componentDao.removeComponent(foodSupplies);
     }
 }
