244085c1958ef5d3b880d8080adefe9227e14527
Martin Kazimir <xkazimir@fi.muni.cz>
2021-04-26 21:00:43 +0200

    MissionCreateDTO implemented

---
+++ rocket-mission-api/src/main/java/cz/muni/fi/dto/MissionCreateDTO.java
@@ -0,0 +1,130 @@
+package cz.muni.fi.dto;
+
+import cz.muni.fi.enums.MissionProgress;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import java.time.ZonedDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * DTO for creating of mission
+ *
+ * @author Martin Kazimir
+ */
+public class MissionCreateDTO {
+
+    @NotNull
+    @Size(min = 2, max = 60)
+    private String name;
+
+    @NotNull
+    @Size(min = 2, max = 255)
+    private String destination;
+
+    @NotNull
+    @Size(min = 1)
+    private Set<UserDTO> users = new HashSet<>();
+
+    @NotNull
+    @Size(min = 1)
+    private Set<RocketDTO> rockets = new HashSet<>();
+
+    @NotNull
+    private Set<ComponentDTO> components = new HashSet<>();
+
+    @NotNull
+    @Future
+    private ZonedDateTime eta;
+
+    private MissionProgress missionProgress;
+
+    private ZonedDateTime finishedDate;
+
+    private ZonedDateTime startedDate;
+
+    private String result;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public Set<UserDTO> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<UserDTO> users) {
+        this.users = users;
+    }
+
+    public Set<RocketDTO> getRockets() {
+        return rockets;
+    }
+
+    public void setRockets(Set<RocketDTO> rockets) {
+        this.rockets = rockets;
+    }
+
+    public Set<ComponentDTO> getComponents() {
+        return components;
+    }
+
+    public void setComponents(Set<ComponentDTO> components) {
+        this.components = components;
+    }
+
+    public ZonedDateTime getEta() {
+        return eta;
+    }
+
+    public void setEta(ZonedDateTime eta) {
+        this.eta = eta;
+    }
+
+    public MissionProgress getMissionProgress() {
+        return missionProgress;
+    }
+
+    public void setMissionProgress(MissionProgress missionProgress) {
+        this.missionProgress = missionProgress;
+    }
+
+    public ZonedDateTime getFinishedDate() {
+        return finishedDate;
+    }
+
+    public void setFinishedDate(ZonedDateTime finishedDate) {
+        this.finishedDate = finishedDate;
+    }
+
+    public ZonedDateTime getStartedDate() {
+        return startedDate;
+    }
+
+    public void setStartedDate(ZonedDateTime startedDate) {
+        this.startedDate = startedDate;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
