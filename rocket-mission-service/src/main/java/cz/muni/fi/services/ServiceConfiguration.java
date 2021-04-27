32d11caebfc6c3a53506484d1ccf400ecda2a3da
Tomas Bouma <tdtom167@gmail.com>
2021-04-27 17:25:31 +0200

    UpdateRocketDTO created

--- rocket-mission-service/src/main/java/cz/muni/fi/services/ServiceConfiguration.java
+++ rocket-mission-service/src/main/java/cz/muni/fi/services/ServiceConfiguration.java
@@ -36,6 +36,7 @@ public class ServiceConfiguration {
             mapping(User.class, UpdateUserDTO.class);
             mapping(Rocket.class, RocketDTO.class);
             mapping(Rocket.class, CreateRocketDTO.class);
+            mapping(Rocket.class, UpdateRocketDTO.class);
         }
     }
 }
