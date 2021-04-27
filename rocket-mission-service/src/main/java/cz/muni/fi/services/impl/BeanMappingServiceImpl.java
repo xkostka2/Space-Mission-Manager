ff7125911903acc11d3b9019338f91bb853f50ef
Martin Kostka <mkostka62@gmail.com>
2021-04-27 09:18:33 +0200

    MissionServiceImpl, UserServiceImpl and BeanMappingServiceImpl moved to right package

--- rocket-mission-service/src/main/java/cz/muni/fi/services/impl/BeanMappingServiceImpl.java
+++ rocket-mission-service/src/main/java/cz/muni/fi/services/impl/BeanMappingServiceImpl.java
@@ -1,5 +1,6 @@
-package cz.muni.fi.services;
+package cz.muni.fi.services.impl;
 
+import cz.muni.fi.services.BeanMappingService;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
