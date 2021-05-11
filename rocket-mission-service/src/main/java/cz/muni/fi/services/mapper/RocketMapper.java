84ec952be2cc41130627bdc6b776ac32e590bf51
xhorelka <m.horelka98@gmail.com>
2021-05-12 00:01:26 +0200

    Introduced MapStruct, removed Dozer

---
+++ rocket-mission-service/src/main/java/cz/muni/fi/services/mapper/RocketMapper.java
@@ -0,0 +1,28 @@
+package cz.muni.fi.services.mapper;
+
+import cz.muni.fi.dto.rocket.CreateRocketDTO;
+import cz.muni.fi.dto.rocket.RocketDTO;
+import cz.muni.fi.dto.rocket.UpdateRocketDTO;
+import cz.muni.fi.entity.Rocket;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+/**
+ * Maps Rocket entity related DTOs to their Entity counterparts.
+ *
+ * @author Martin Hořelka (469003)
+ */
+@Mapper(componentModel = "spring")
+public interface RocketMapper {
+    RocketDTO rocketToRocketDTO(Rocket rocket);
+    Rocket rocketDTOToRocket(RocketDTO rocketDTO);
+
+    CreateRocketDTO rocketToCreateRocketDTO(Rocket rocket);
+    Rocket createRocketDTOToRocket(CreateRocketDTO createRocketDTO);
+
+    UpdateRocketDTO rocketToUpdateRocketDTO(Rocket rocket);
+    Rocket updateRocketDTOToRocket(UpdateRocketDTO updateRocketDTO);
+
+    List<RocketDTO> rocketsToRocketDTOs(List<Rocket> rockets);
+}
---
+++ rocket-mission-service/src/main/java/cz/muni/fi/services/mapper/UserMapper.java
@@ -0,0 +1,28 @@
+package cz.muni.fi.services.mapper;
+
+import cz.muni.fi.dto.user.CreateUserDTO;
+import cz.muni.fi.dto.user.UpdateUserDTO;
+import cz.muni.fi.dto.user.UserDTO;
+import cz.muni.fi.entity.User;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+/**
+ * Maps User entity related DTOs to their Entity counterparts.
+ *
+ * @author Martin Hořelka (469003)
+ */
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserDTO userToUserDTO(User user);
+    User userDTOToUser(UserDTO userDTO);
+
+    CreateUserDTO userToCreateUserDTO(User user);
+    User createUserDTOToUser(CreateUserDTO createUserDTO);
+
+    UpdateUserDTO userToUpdateUserDTO(User user);
+    User updateUserDTOToUser(UpdateUserDTO updateUserDTO);
+
+    List<UserDTO> usersToUserDTOs(List<User> users);
+}
