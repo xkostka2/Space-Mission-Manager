23413e6b12e1c858163ed891fc93501bba19911d
Tomas Bouma <tdtom167@gmail.com>
2021-05-14 16:52:50 +0200

    CRUD USER rest

---
+++ rocket-mission-rest/src/main/java/cz/muni/fi/controllers/UserController.java
@@ -0,0 +1,93 @@
+package cz.muni.fi.controllers;
+
+import cz.muni.fi.ApiUris;
+import cz.muni.fi.dto.user.CreateUserDTO;
+import cz.muni.fi.dto.user.UpdateUserDTO;
+import cz.muni.fi.dto.user.UserDTO;
+import cz.muni.fi.exceptions.ResourceAlreadyExistsException;
+import cz.muni.fi.exceptions.ResourceNotFoundException;
+import cz.muni.fi.facade.RocketFacade;
+import cz.muni.fi.facade.UserFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping(ApiUris.ROOT_URI_USERS)
+public class UserController {
+
+    private final static Logger logger = Logger.getLogger(ComponentController.class.getName());
+
+    private final UserFacade userFacade;
+
+
+    @Autowired
+    public UserController(UserFacade userFacade) {
+        this.userFacade = userFacade;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserDTO createComponent(@RequestBody CreateUserDTO userCreateDTO) {
+
+        logger.log(Level.INFO, "[REST] creating user");
+
+        try {
+            return userFacade.findUserById(userFacade.addUser(userCreateDTO).getId());
+        } catch (Exception e) {
+            logger.log(Level.WARNING, e.getMessage());
+            throw new ResourceAlreadyExistsException();
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<UserDTO> findAllUsers() {
+
+        logger.log(Level.INFO, "[REST] finding all users");
+
+        return userFacade.findAllUsers();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserDTO findUserById(@PathVariable("id") Long id) {
+
+        logger.log(Level.INFO, "[REST] finding user " + id);
+
+        UserDTO userDTO = userFacade.findUserById(id);
+        if (userDTO == null) {
+            throw new ResourceNotFoundException();
+        }
+        return userDTO;
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserDTO updateUser(@RequestBody UpdateUserDTO updateUserDTO) {
+
+        logger.log(Level.INFO, "[REST] updating user" + updateUserDTO.getId());
+
+        try {
+            userFacade.updateUser(updateUserDTO);
+            return userFacade.findUserById(updateUserDTO.getId());
+        } catch (Exception e) {
+            logger.log(Level.WARNING, e.getMessage());
+            throw new ResourceAlreadyExistsException();
+        }
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<UserDTO> removeUser(@PathVariable("id") long id) {
+
+        logger.log(Level.INFO, "[REST] deleting user" + id);
+
+        UserDTO user = userFacade.findUserById(id);
+        if (user == null) {
+            throw new ResourceNotFoundException();
+        }
+
+        userFacade.deleteUser(user);
+        return userFacade.findAllUsers();
+    }
+}
