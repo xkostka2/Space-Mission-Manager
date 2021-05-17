a0628adcc397e58038a838c14299f92c9e6a92c2
Martin Kazimir <xkazimir@fi.muni.cz>
2021-05-17 13:27:39 +0200

    REST user implemented

--- rocket-mission-rest/src/main/java/cz/muni/fi/controllers/UserController.java
+++ rocket-mission-rest/src/main/java/cz/muni/fi/controllers/UserController.java
@@ -6,7 +6,6 @@ import cz.muni.fi.dto.user.UpdateUserDTO;
 import cz.muni.fi.dto.user.UserDTO;
 import cz.muni.fi.exceptions.ResourceAlreadyExistsException;
 import cz.muni.fi.exceptions.ResourceNotFoundException;
-import cz.muni.fi.facade.RocketFacade;
 import cz.muni.fi.facade.UserFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -51,12 +50,40 @@ public class UserController {
         return userFacade.findAllUsers();
     }
 
+    @RequestMapping(value = "/astronauts", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<UserDTO> findAllAstronauts() {
+
+        logger.log(Level.INFO, "[REST] finding all astronauts");
+
+        return userFacade.findAllAstronauts();
+    }
+
+    @RequestMapping(value = "/astronauts/available", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<UserDTO> findAllAvailableAstronauts() {
+
+        logger.log(Level.INFO, "[REST] finding all available astronauts");
+
+        return userFacade.findAllAvailableAstronauts();
+    }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public UserDTO findUserById(@PathVariable("id") Long id) {
 
         logger.log(Level.INFO, "[REST] finding user " + id);
 
         UserDTO userDTO = userFacade.findUserById(id);
+        if (userDTO == null) {
+            throw new ResourceNotFoundException();
+        }
+        return userDTO;
+    }
+
+    @RequestMapping(value = "/email", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserDTO findUserByEmail(@RequestBody String email) {
+
+        logger.log(Level.INFO, "[REST] finding user with email " + email);
+
+        UserDTO userDTO = userFacade.findUserByEmail(email);
         if (userDTO == null) {
             throw new ResourceNotFoundException();
         }
@@ -89,5 +116,32 @@ public class UserController {
 
         userFacade.deleteUser(user);
         return userFacade.findAllUsers();
+    }
+
+    @RequestMapping(value = "/{id}/acceptMission", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserDTO acceptMission(@PathVariable Long id) {
+
+        logger.log(Level.INFO, "[REST] accept mission by user with id " + id);
+
+        UserDTO user = userFacade.findUserById(id);
+        if (user == null) {
+            throw new ResourceNotFoundException();
+        }
+
+        userFacade.acceptAssignedMission(user);
+        return userFacade.findUserById(id);
+    }
+
+    @RequestMapping(value = "{id}/rejectMission", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserDTO rejectMission(@PathVariable Long id, @RequestBody String explanation) {
+
+        logger.log(Level.INFO, "[REST] reject mission by user with id " + id);
+
+        UserDTO user = userFacade.findUserById(id);
+        if (user == null) {
+            throw new ResourceNotFoundException();
+        }
+        userFacade.rejectAssignedMission(user, explanation);
+        return userFacade.findUserById(id);
     }
 }
