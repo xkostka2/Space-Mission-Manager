0c6b4f407e5090366a02fe84906ecd3b16e2827d
Tomas Bouma <tdtom167@gmail.com>
2021-05-19 17:07:37 +0200

    SECURITY WIP

---
+++ rocket-mission-api/src/main/java/cz/muni/fi/dto/user/PersonAuthDto.java
@@ -0,0 +1,40 @@
+package cz.muni.fi.dto.user;
+
+/**
+ * @author Tomas Bouma
+ */
+public class PersonAuthDto {
+
+    private String email;
+    private String password;
+
+    public PersonAuthDto(){}
+
+    public PersonAuthDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonAuthDto{" +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
