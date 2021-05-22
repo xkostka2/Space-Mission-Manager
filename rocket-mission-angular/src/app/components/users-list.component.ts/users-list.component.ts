773395199d4215005442e4a0674830918fa00f5e
xhorelka <m.horelka98@gmail.com>
2021-05-22 16:39:58 +0200

    Users list component

---
+++ rocket-mission-angular/src/app/components/users-list.component.ts/users-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
+import { MatTableDataSource } from "@angular/material";
+import { User } from "src/app/models/user";
+
+@Component({
+    selector: 'app-users-list',
+    templateUrl: './users-list.component.html',
+    styleUrls: ['./users-list.component.scss']
+  })
+  export class UsersListComponent implements OnChanges {
+
+    @Input() users: User[];
+    @Input() displayedColumns: String[];
+
+    dataSource = new MatTableDataSource<User>();
+
+    ngOnChanges(): void {
+        this.dataSource = new MatTableDataSource<User>(this.users);
+    }
+  }
