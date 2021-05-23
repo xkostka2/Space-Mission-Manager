1b0f047a42a104fcc0208fc54cb7614e50ae0899
Tomas Bouma <tdtom167@gmail.com>
2021-05-23 14:16:53 +0200

    Run everything on port 8080

---
+++ rocket-mission-manager/src/main/java/cz/muni/fi/Application.java
@@ -0,0 +1,8 @@
+package cz.muni.fi;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class Application {
+}
