63d304b3ef5fbb8d0b2f41e41232d6558ed942dd
xhorelka <m.horelka98@gmail.com>
2021-06-05 00:50:44 +0200

    mission detail page

--- rocket-mission-angular/src/app/pages/mission-detail-page/mission-detail-page.component.ts
+++ rocket-mission-angular/src/app/pages/mission-detail-page/mission-detail-page.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MissionComponent } from 'src/app/models/component';
+import { Mission } from 'src/app/models/mission';
+import { Rocket } from 'src/app/models/rocket';
+import { Role } from 'src/app/models/role';
+import { User } from 'src/app/models/user';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { MissionsService } from 'src/app/services/missions.service';
 
 @Component({
   selector: 'app-mission-detail-page',
@@ -7,9 +15,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MissionDetailPageComponent implements OnInit {
 
-  constructor() { }
+  private mission_id: number;
+
+  mission: Mission;
+  astronautLogged: boolean;
+
+  constructor(
+    private route: ActivatedRoute,
+    private missionService: MissionsService,
+    private authenticationService: AuthenticationService,
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.mission_id = params['id'];
+    })
+
+    this.missionService.findMissionById(this.mission_id).subscribe(x => {
+      this.mission = x;
+    })
+
+    this.astronautLogged = this.authenticationService.getRole() === Role.Astronaut;
   }
 
+  getUsersList(users: User[]): string {
+    return users.length === 0 ? "None" : users.map(x => x.name).join(", ");
+  }
+
+  getRocketsList(rockets: Rocket[]): string {
+    return rockets.length === 0 ? "None" : rockets.map(x => x.name).join(", ");
+  }
+
+  getComponentsList(components: MissionComponent[]): string {
+    return components.length === 0 ? "None" : components.map(x => x.name).join(", ");
+  }
 }
